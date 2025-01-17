82c558ef2e72aadf6b5125e64ffd2a0b58a71d90
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2025-01-17 23:21:57 +0100

    Added cakes to responsive nav menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,7 +38,6 @@
                             </div>
                         </button>
                     </x-slot>
-
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
@@ -75,6 +74,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('cakes.index')" :active="request()->routeIs('cakes.index')">
+                {{ __('Cakes') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('cakes.create')" :active="request()->routeIs('cakes.create')">
+                {{ __('Add cakes') }}
             </x-responsive-nav-link>
         </div>
 
