46fb4b3ae50b4efcd943d36514d50aa951f282ea
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2025-01-18 00:43:48 +0100

    Added feedback functionality

--- database/migrations/2025_01_17_222332_create_feedback_table.php
+++ database/migrations/2025_01_17_222332_create_feedback_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('feedback', function (Blueprint $table) {
+        Schema::create('cakes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('message');
+            $table->string('name');
+            $table->integer('price');
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
