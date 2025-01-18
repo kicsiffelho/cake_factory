c64b5df41a5047acbeb9dd3cfa4669c78440efd6
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2025-01-18 02:46:40 +0100

    Updated welcome page

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -8,7 +8,7 @@
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Send') }}</x-primary-button>
+            <button class="mt-4 bg-black text-white px-4 py-2 rounded-lg">{{ __('Send') }}</button>
         </form>
     </div>
 </x-app-layout>
